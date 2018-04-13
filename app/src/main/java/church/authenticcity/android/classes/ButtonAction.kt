89268bfcc9b620206c67d29537798add286f1266
Greg Whatley <devgregw@outlook.com>
2018-04-13 10:54:12 -0500

    Added add to calendar action
    Also added show map action and improve error handling

--- app/src/main/java/church/authenticcity/android/classes/ButtonAction.kt
+++ app/src/main/java/church/authenticcity/android/classes/ButtonAction.kt
@@ -1,38 +1,117 @@
 package church.authenticcity.android.classes
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.provider.CalendarContract
+import android.provider.Settings
 import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.widget.Toast
+import church.authenticcity.android.EventActivity
 import church.authenticcity.android.TabActivity
+import church.authenticcity.android.helpers.RecurrenceRule
+import church.authenticcity.android.helpers.Utils
 import church.authenticcity.android.helpers.applyColorsAndTypefaces
+import church.authenticcity.android.helpers.isNullOrWhiteSpace
+import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZonedDateTime
+import org.threeten.bp.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.HashMap
 
 class ButtonAction(private val map: HashMap<String, Any>) {
-    var group: Int = -1
-        get() = map["group"].toString().toInt()
-    var type: String = ""
-        get() = map["type"].toString()
-    var properties: HashMap<String, Any> = HashMap()
-        get() = HashMap(map.filter { it.key != "group" && it.key != "type" })
+    val group = map["group"].toString().toInt()
+    val type = map["type"] as String
+    var properties = HashMap<String, Any>(map.filter { it.key != "group" && it.key != "type" })
 
     fun <T> get(name: String): T = properties[name] as T
 
+    /*val isExternal =
+            when (type) {
+                "OpenTabAction", "OpenEventAction" -> false
+                else -> true
+            }*/
+
     fun invoke(context: Context) {
-        Log.v("ButtonAction", String.format("Invoking %s", map))
-        when (type) {
-            "OpenTabAction" -> TabActivity.start(context, get<String>("tabId"))
-            "OpenURLAction" -> {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(get<String>("url")))
-                when (context.packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).count()) {
-                    0 -> context.startActivity(intent)
-                    else -> context.startActivity(Intent.createChooser(intent, "Choose an app"))
+        try {
+            Log.v("ButtonAction", String.format("Invoking %s", map))
+            when (type) {
+                "OpenTabAction" -> TabActivity.start(context, get<String>("tabId"))
+                "OpenEventAction" -> EventActivity.start(context, get<String>("eventId"))
+                "OpenURLAction" -> {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(get<String>("url")))
+                    when (context.packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).count()) {
+                        0 -> AlertDialog.Builder(context).setTitle("Cannot Open URL").setMessage("We could not find an app to open the URL \"${get<String>("url")}\".").setPositiveButton("OK", null).create().applyColorsAndTypefaces().show()
+                        1 -> context.startActivity(intent)
+                        else -> context.startActivity(Intent.createChooser(intent, "Choose an app to open this"))
+                    }
                 }
+                "ShowMapAction" -> context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + Uri.encode(get<String>("address")))).apply { `package` = "com.google.android.apps.maps" })
+                "GetDirectionsAction" -> context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q=" + Uri.encode(get<String>("address")))).apply { `package` = "com.google.android.apps.maps" })
+                "EmailAction" -> context.startActivity(Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + get<String>("emailAddress"))))
+                "AddToCalendarAction" -> {
+                    val eventTitle: String
+                    val startDate: ZonedDateTime
+                    val endDate: ZonedDateTime
+                    val location: String
+                    val recurrenceRule: RecurrenceRule?
+                    when (group) {
+                        0 -> {
+                            val event = Utils.Temp.getEvent(get("eventId"))!!
+                            eventTitle = event.title
+                            startDate = event.startDate
+                            endDate = event.endDate
+                            location = if (!String.isNullOrWhiteSpace(event.address)) event.address else event.location
+                            recurrenceRule = event.recurrenceRule
+                        }
+                        1 -> {
+                            eventTitle = get("title")
+                            val dates = get<HashMap<String, Any>>("dateTime")
+                            startDate = OffsetDateTime.parse(dates["start"] as String, DateTimeFormatter.ISO_DATE_TIME).atZoneSameInstant(ZoneId.systemDefault())
+                            endDate = OffsetDateTime.parse(dates["end"] as String, DateTimeFormatter.ISO_DATE_TIME).atZoneSameInstant(ZoneId.systemDefault())
+                            location = get("location")
+                            recurrenceRule = null
+                        }
+                        else -> throw IllegalStateException("Group $group is not legal for AddToCalendarAction")
+                    }
+                    val uri = Uri.parse("content://com.android.calendar/events")
+                    val values = ContentValues().apply {
+                        put(CalendarContract.Events.CALENDAR_ID, 1)
+                        put(CalendarContract.Events.TITLE, eventTitle)
+                        put(CalendarContract.Events.EVENT_LOCATION, location)
+                        put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().id)
+                        val startMillis = startDate.toEpochSecond() * 1000
+                        val endMillis = endDate.toEpochSecond() * 1000
+                        put(CalendarContract.Events.DTSTART, startMillis)
+                        if (recurrenceRule == null)
+                            put(CalendarContract.Events.DTEND, endMillis)
+                        else {
+                            put(CalendarContract.Events.DURATION, "P${(endMillis - startMillis) / 1000}S")
+                            put(CalendarContract.Events.RRULE, recurrenceRule.getRRule())
+                        }
+                    }
+                    try {
+                        context.contentResolver.insert(uri, values)
+                        Utils.makeToast(context, "\"$eventTitle\" was added to your calendar.", Toast.LENGTH_LONG).show()
+                    } catch (ex: SecurityException) {
+                        ex.printStackTrace()
+                        AlertDialog.Builder(context).setTitle("Permission Denied").setMessage("\"$eventTitle\" could not be added to your calendar because you didn't grant the Authentic app access to your calendar.").setNeutralButton("Settings", { _, _ ->
+                            context.startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:${context.packageName}")).apply {
+                                addCategory(Intent.CATEGORY_DEFAULT)
+                                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            })
+                        }).setPositiveButton("Dismiss", null).create().applyColorsAndTypefaces().show()
+                    }
+                }
+                else -> throw IllegalStateException("\"$type\" is not a legal action")//AlertDialog.Builder(context).setTitle("Unknown Action").setMessage("We were unable to parse this action.").setPositiveButton("OK", null).create().applyColorsAndTypefaces().show()
             }
-            "GetDirectionsAction" -> context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q=" + get<String>("address"))).apply { `package` = "com.google.android.apps.maps" })
-            "EmailAction" -> context.startActivity(Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + get<String>("emailAddress"))))
-            else -> AlertDialog.Builder(context).setTitle("Unknown Action").setMessage("We were unable to parse this action.").setPositiveButton("OK", null).create().applyColorsAndTypefaces().show()//Toast.makeText(context, String.format("Group: %s, Type: %s, Props: %s", group, type, properties), Toast.LENGTH_LONG).show()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            Utils.reportAndAlertException(context, ex, this.javaClass.getMethod("invoke", Context::class.java).toString())
         }
     }
 }
--- app/src/main/java/church/authenticcity/android/helpers/Utils.kt
+++ app/src/main/java/church/authenticcity/android/helpers/Utils.kt
@@ -1,8 +1,9 @@
 package church.authenticcity.android.helpers
 
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
+import android.graphics.PorterDuff
 import android.graphics.Typeface
 import android.os.Build
 import android.support.v4.content.res.ResourcesCompat
@@ -10,13 +11,18 @@ import android.support.v7.app.ActionBar
 import android.support.v7.app.AlertDialog
 import android.text.Spannable
 import android.text.SpannableString
-import android.widget.ImageView
-import android.widget.TextView
+import android.util.TypedValue
+import android.view.ViewGroup
+import android.widget.*
 import church.authenticcity.android.BuildConfig
 import church.authenticcity.android.R
+import church.authenticcity.android.classes.AuthenticEvent
+import church.authenticcity.android.classes.AuthenticTab
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.firebase.crash.FirebaseCrash
 import com.google.firebase.storage.FirebaseStorage
+import org.threeten.bp.format.DateTimeFormatter
 
 fun String.Companion.isNullOrWhiteSpace(string: String?): Boolean {
     if (string != null) {
@@ -52,15 +58,70 @@ fun AlertDialog.applyColorsAndTypefaces(): AlertDialog {
 }
 
 fun ActionBar.applyTypeface(context: Context, text: String) {
-    val span = SpannableString(text)
-    span.setSpan(TypefaceSpan(Utils.getTitleTypeface(context)), 0, span.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-    this.title = span
+    this.title = Utils.makeTypefaceSpan(text, Utils.getTitleTypeface(context))
 }
 
+inline fun <reified T, V, K> HashMap<V, K>.getAs(key: V) = (if (T::class.simpleName == Int::class.simpleName) this[key].toString().toInt() else if (T::class.simpleName == Float::class.simpleName) this[key].toString().toFloat() else this[key]) as T
+
 class Utils {
+    class Temp {
+        companion object {
+            private var objects: HashMap<String, Any> = HashMap()
+
+            fun getTab(id: String): AuthenticTab? = if (objects.containsKey(id)) objects[id] as AuthenticTab else null
+
+            fun getEvent(id: String): AuthenticEvent? = if (objects.containsKey(id)) objects[id] as AuthenticEvent else null
+
+            fun putTab(tab: AuthenticTab) {
+                objects[tab.id] = tab
+            }
+
+            fun putEvent(event: AuthenticEvent) {
+                objects[event.id] = event
+            }
+        }
+    }
+
+    class Constructors {
+        companion object {
+            fun constructEvent(value: Any): AuthenticEvent {
+                val map = value as HashMap<String, Any>
+                return AuthenticEvent(map.getAs("id"), map.getAs("title"), map.getAs("hideTitle"), map.getAs("description"), map.getAs("header"), map.getAs("dateTime"), map.getAs("recurrence"), map.getAs("location"), map.getAs("address"), map.getAs("registration"))
+            }
+
+            fun constructTab(value: Any): AuthenticTab {
+                val map = value as HashMap<String, Any>
+                return AuthenticTab(map.getAs("header"), map.getAs("id"), map.getAs("index"), map.getAs("hideTitle"), map.getAs("hideHeader"), map.getAs("title"), map.getAs("elements"), map.getAs("visibility"))
+            }
+        }
+    }
+
     companion object {
+        val datePattern
+            get() = DateTimeFormatter.ofPattern("EEEE, MMMM d, y")!!
+        val timePattern
+            get() = DateTimeFormatter.ofPattern("h:mm a")!!
+
         private var title: Typeface? = null
         private var text: Typeface? = null
+
+        fun reportAndAlertException(context: Context, ex: Exception, location: String) {
+            FirebaseCrash.report(ex)
+            var msg = "Unfortunately, an unexpected error occurred and has been reported.  We apologize for the inconvenience.  If you need them, here are the details:\n"
+            msg += "\nLocation: $location"
+            msg += "\nType: ${ex.javaClass.canonicalName ?: "<null>"}"
+            msg+= "\nMessage: ${ex.message ?: "<null>"}"
+            AlertDialog.Builder(context).setTitle("Unexpected Error").setMessage(msg).setPositiveButton("Dismiss", null).create().applyColorsAndTypefaces().show()
+        }
+
+        @SuppressLint("ShowToast")
+        fun makeToast(context: Context, text: String, length: Int) = Toast.makeText(context, makeTypefaceSpan(text, Utils.getTextTypeface(context)), length)!!
+
+        fun makeTypefaceSpan(text: String, typeface: Typeface): SpannableString {
+            val span = SpannableString(text)
+            span.setSpan(TypefaceSpan(typeface), 0, span.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            return span
+        }
 
         fun getTitleTypeface(context: Context): Typeface {
             if (title == null)
@@ -80,10 +141,35 @@ class Utils {
 
         fun isUpdateAvailable(latestCode: Int) = BuildConfig.VERSION_CODE < latestCode
 
-        fun showErrorDialog(activity: Activity, code: Int, message: String, details: String) {
-            AlertDialog.Builder(activity).setCancelable(false).setTitle("Error").setMessage(String.format("Unfortunately, an error occurred.\nCode: %s\nMessage: %s\nDetails: %s", code, message, details)).setPositiveButton("Close", { _, _ -> activity.finish() }).create().apply { setOnShowListener { dialog -> (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.WHITE) } }.show()
-        }
-
         fun checkSdk(apiLevel: Int): Boolean = Build.VERSION.SDK_INT >= apiLevel
+
+        fun createIndeterminateDialog(context: Context, message: String): AlertDialog {
+            return AlertDialog.Builder(context).setView(RelativeLayout(context).apply {
+                setBackgroundColor(Color.parseColor("#212121"))
+                val padding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16f, resources.displayMetrics).toInt()
+                setPadding(padding, padding, padding, padding)
+                addView(ProgressBar(context).apply {
+                    isIndeterminate = true
+                    val size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50f, resources.displayMetrics).toInt()
+                    id = R.id.content_list
+                    layoutParams = RelativeLayout.LayoutParams(size, size).apply {
+                        addRule(RelativeLayout.ALIGN_PARENT_LEFT)
+                        addRule(RelativeLayout.CENTER_VERTICAL)
+                    }
+                    indeterminateDrawable.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN)
+                })
+                addView(TextView(context).apply {
+                    text = message
+                    typeface = getTextTypeface(context)
+                    setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
+                    layoutParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT).apply {
+                        setMargins(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics).toInt(), 0, 0, 0)
+                        addRule(RelativeLayout.RIGHT_OF, R.id.content_list)
+                        addRule(RelativeLayout.CENTER_VERTICAL)
+                    }
+                    setTextColor(Color.WHITE)
+                })
+            }).setCancelable(false).create().applyColorsAndTypefaces()
+        }
     }
 }
