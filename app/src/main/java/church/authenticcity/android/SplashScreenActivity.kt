e4b122ae0c45598c5e5474b91b4a1b1730e04fea
Greg Whatley <devgregw@outlook.com>
2018-04-13 10:56:00 -0500

    Added offline support

--- app/src/main/java/church/authenticcity/android/SplashScreenActivity.kt
+++ app/src/main/java/church/authenticcity/android/SplashScreenActivity.kt
@@ -3,10 +3,12 @@ package church.authenticcity.android
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
