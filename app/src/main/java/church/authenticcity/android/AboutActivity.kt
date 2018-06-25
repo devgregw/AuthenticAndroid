5366110c7ccef6452b4818989890ac9daa5d01c5
Greg Whatley <devgregw@outlook.com>
2018-06-25 10:08:27 -0500

    Added legal docs and licenses

--- app/src/main/java/church/authenticcity/android/AboutActivity.kt
+++ app/src/main/java/church/authenticcity/android/AboutActivity.kt
@@ -1,12 +1,16 @@
 package church.authenticcity.android
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.TypedValue
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -17,7 +21,9 @@ import church.authenticcity.android.helpers.applyColorsAndTypefaces
 import church.authenticcity.android.helpers.applyTypeface
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.google.firebase.messaging.FirebaseMessaging
+import kotlin.math.roundToInt
 
 class AboutActivity : AppCompatActivity() {
 
@@ -85,6 +91,22 @@ class AboutActivity : AppCompatActivity() {
             addView(AuthenticElement.createButton(this@AboutActivity, ButtonAction.openUrl("https://devgregw.com"), "Learn More"))
             addView(AuthenticElement.createButton(this@AboutActivity, ButtonAction.openUrl("https://github.com/devgregw/AuthenticAndroid"), "GitHub Repository"))
             addView(AuthenticElement.createButton(this@AboutActivity, ButtonAction.openUrl("https://trello.com/b/QUgekVh6"), "Trello Roadmap"))
+            addView(AuthenticElement.createSeparator(this@AboutActivity, true))
+            addView(AuthenticElement.createButton(this@AboutActivity, ButtonAction.openUrl("https://docs.accams.devgregw.com"), "Legal Documentation"))
+            addView(AuthenticElement.createButton(this@AboutActivity, ButtonAction.openUrl("https://docs.accams.devgregw.com/privacy-policy"), "Privacy Policy"))
+            OssLicensesMenuActivity.setActivityTitle("Licenses")
+            addView(Button(context).apply {
+                this.text = "Licenses"
+                val dimen = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, context.resources.displayMetrics).roundToInt()
+                setPadding(dimen, dimen, dimen, dimen)
+                setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+                typeface = Utils.getTextTypeface(context)
+                setOnClickListener { this@AboutActivity.startActivity(Intent(this@AboutActivity, OssLicensesMenuActivity::class.java)) }
+                layoutParams = ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT).apply {
+                    val px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16f, context.resources.displayMetrics).toInt()
+                    setMargins(px, 0, px, 0)
+                }
+            })
             addView(AuthenticElement.createSeparator(this@AboutActivity, false))
         }
     }
