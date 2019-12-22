ccaea02535514b745595098c0871210c00925a84
Greg Whatley <me@gregwhatley.dev>
2019-12-22 12:20:31 -0600

    Update AuthenticAppearance.kt

--- app/src/main/java/church/authenticcity/android/classes/AuthenticAppearance.kt
+++ app/src/main/java/church/authenticcity/android/classes/AuthenticAppearance.kt
@@ -6,17 +6,29 @@ package church.authenticcity.android.classes
  * Licensed under the MIT License.
  */
 class AuthenticAppearance(data: HashMap<String, Any>) {
+    companion object {
+        val default = AuthenticAppearance(HashMap<String, Any>().apply {
+            put("tabs", HashMap<String, Any>())
+            put("events", HashMap<String, Any>())
+            put("livestream", HashMap<String, Any>())
+        })
+    }
 
     class Events(data: HashMap<String, Any>) {
-        val title: String = data["title"] as String
-        val header: ImageResource = ImageResource(data["header"] as HashMap<String, Any>)
-
+        val title: String = data["title"] as? String ?: "UPCOMING EVENTS"
+        val header: ImageResource = if (data.containsKey("header")) ImageResource(data["header"] as HashMap<String, Any>) else ImageResource("unknown.png", 1920, 1080)
+        val index: Int = data["index"] as? Int ?: -999
     }
 
     class Tabs(data: HashMap<String, Any>) {
         val fill = if (data.containsKey("fill")) data["fill"].toString().toBoolean() else true
     }
 
+    class Livestream(data: HashMap<String, Any>) {
+        val enable = (data["enable"] ?: "true").toString().toBoolean()
+    }
+
     val events = Events(data["events"] as HashMap<String, Any>)
     val tabs = Tabs(data["tabs"] as HashMap<String, Any>)
+    val livestream = Livestream(data["livestream"] as HashMap<String, Any>)
 }
